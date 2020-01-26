25d76b2b19c0b0bd3d33b341a68fd628503c6c2d
Cheng-Lun Li <allen79624@gmail.com>
2020-01-26 17:06:09 +0000

    changed some code

--- reparse.sh
+++ reparse.sh
@@ -3,7 +3,7 @@
 # $1 = 07 # 2007
 
 # Decompress files from archive
-tar -C edgar-10k-mda -xvf edgar-10k-mda/form10k$1.tar.gz 
+#tar -C edgar-10k-mda -xvf edgar-10k-mda/form10k$1.tar.gz 
 
 # Extract item 1 and item 7 from files
 python3 edgar-10k-mda/edgar.py extract mda --10k-dir=edgar-10k-mda/form10k$1 --mda-dir=edgar-10k-mda/form10k$1
@@ -13,7 +13,7 @@ python3 Parsing.py edgar-10k-mda/form10k$1_item1
 python3 Parsing.py edgar-10k-mda/form10k$1_item7
 
 # Remove original decompressed files
-rm -r edgar-10k-mda/form10k$1
+#rm -r edgar-10k-mda/form10k$1
 #rm -r edgar-10k-mda/form10k$1_item1
 #rm -r edgar-10k-mda/form10k$1_item7
 
